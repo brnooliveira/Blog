5a57214c789ee61f250cc3a707017334fcc75730
brnooliveira <brno.oliveiras@gmail.com>
2022-12-07 19:54:26 -0300

    Add: Log and Test: Creates with EF

--- Program.cs
+++ Program.cs
@@ -1,15 +1,56 @@
 ﻿using Blog.Context;
 using Blog.Models;
+using Microsoft.EntityFrameworkCore;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        using (var context = new AppDbContext())
-        {
-            var tag = new Tag { Name = "ASP.NET", Slug = "aspnet" };
-            context.Tags.Add(tag);
-            context.SaveChanges();
-        }
+        using var context = new AppDbContext();
+
+        // var user = new User
+        // {
+        //     Name = "Breno Oliviera",
+        //     Slug = "brenooliveira",
+        //     Email = "breno@example.com",
+        //     Bio = "Estudante Dotnet",
+        //     Image = "site",
+        //     PasswordHash = "12345678"
+        // };
+
+        // var category = new Category
+        // {
+        //     Name = "Backend",
+        //     Slug = "backend"
+        // };
+
+        // var post = new Post
+        // {
+        //     Author = user,
+        //     Category = category,
+        //     Body = "<p>Hello World</p>",
+        //     Slug = "comecandocomefcore",
+        //     Summary = "Aprendendo Entity Framework Core",
+        //     Title = "Comecando com EF Core",
+        //     CreateDate = DateTime.Now,
+        //     LastUpdateDate = DateTime.Now
+        // };
+
+        // context.Posts.Add(post);
+        // context.SaveChanges();
+
+        // var posts = context.Posts.AsNoTracking().Include(x => x.Author).Include(x => x.Category).OrderByDescending(x => x.LastUpdateDate).ToList();
+
+        // foreach (var post in posts)
+        //     Console.WriteLine($"{post.Title} escrito por {post.Author.Name} em {post.Category.Name}");
+
+        var post = context.Posts.Include(x => x.Author).Include(x => x.Category).OrderByDescending(x => x.LastUpdateDate).FirstOrDefault();
+
+        post.Author.Name = "Teste";
+
+        context.Posts.Update(post);
+        context.SaveChanges();
+
+
     }
 }
